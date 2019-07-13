3c012d314a6e2773edd4b56aeae94148249dc12c
Terry Moore <tmm@mcci.com>
2019-07-13 18:59:26 -0400

    Trim whitespace

--- src/mcciadk_env.h
+++ src/mcciadk_env.h
@@ -93,7 +93,7 @@ Description:
 	error. The results of using this macro where a declaration is not
 	permitted are unspecified.
 
-	This is different from #if !(fErrorIfFalse) / #error in that the 
+	This is different from #if !(fErrorIfFalse) / #error in that the
 	expression is evaluated by the compiler rather than by the pre-
 	processor. Therefore things like sizeof() can be used.
 
@@ -134,7 +134,7 @@ Function:
 	intentionally unused.
 
 Definition:
-	MCCIADK_UNREFERENCED_PARAMETER(name)	
+	MCCIADK_UNREFERENCED_PARAMETER(name)
 	MCCIADK_API_PARAMETER(name)
 	MCCIADK_UNREFERENCED_VARIABLE(name)
 
